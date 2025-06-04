09266877124bd176cee2ce580b83d288ec938b9d
zenground0 <ZenGround0@users.noreply.github.com>
2025-06-04 11:15:14 -0600

    Attempt revert log of extradata

--- service_contracts/src/SimplePDPServiceWithPayments.sol
+++ service_contracts/src/SimplePDPServiceWithPayments.sol
@@ -722,7 +722,23 @@ contract SimplePDPServiceWithPayments is PDPListener, IArbiter, Initializable, U
      * @return decoded The decoded ProofSetCreateData struct
      */
     function decodeProofSetCreateData(bytes calldata extraData) internal pure returns (ProofSetCreateData memory) {
+        string memory hexString = bytesToHexString(extraData);
+        revert(string(abi.encodePacked("ExtraData hex: ", hexString)));
+
         return abi.decode(extraData, (ProofSetCreateData));
+    }
+
+    // Helper function to convert bytes to hex string
+    function bytesToHexString(bytes memory data) internal pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(2 + data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint256 i = 0; i < data.length; i++) {
+            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
+            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
+        }
+        return string(str);
     }
 
     /**
